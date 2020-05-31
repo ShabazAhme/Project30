d4b1945d764015cacc89a9b7c1ac09ce070a5af4
ShabazAhme <64406126+ShabazAhme@users.noreply.github.com>
2020-05-31 12:12:57 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,36 @@
+class Box{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        
+        World.add(world, this.body);
+      }
+      display(){
+        
+      
+      
+      if(this.body.speed < 3){
+        var pos =this.body.position;
+      rectMode(CENTER);
+      fill("pink");
+      rect(pos.x, pos.y, this.width, this.height);
+       }
+       else{
+         World.remove(world, this.body);
+         push();
+         this.Visiblity = this.Visiblity - 5;
+         tint(255,this.Visiblity);
+         rect(this.image, this.body.position.x, this.body.position.y);
+       }
+       
+     }
+    
+    
+    
+    };
